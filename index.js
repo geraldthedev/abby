627a9f8d316d95e5e0dc159cb688f7fc9e1b1d14
Gerald Bryant <hakeem1106@gmail.com>
2019-01-13 19:54:58 +0000

    text loading

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-document.body.onload = rowMaker;
+document.body.onload = rowMaker, rowMaker;
 
 function rowMaker(){
-var name = document.createElement("div");
-var text = document.createTextNode("Text loaded");
+var name1 = document.createElement("div");
+var text1 = document.createTextNode("Text loaded");
 
-name.appendChild('text');
+name1.appendChild(text1);
+
+var node1 = document.getElementById("div1");
+
+document.body.insertBefore(name1, node1);
 }
 
 
