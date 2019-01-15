f9963f6ce7dad8611cd732c695f57fe8353373b0
Gerald Bryant <hakeem1106@gmail.com>
2019-01-15 19:39:46 +0000

    fixed....for now

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ document.body.onload = inputMaker;
 function inputMaker(){
 var node1 = document.getElementById("div1");
 var name1 = document.createElement("input");
+
 var button = document.createElement("button");
-var banner = document.createElement("span").setAttribute("id","banner");
+
 
 
 node1.appendChild(name1);
 node1.appendChild(button);
-node1.appendChild(banner);
+
+
 button.innerHTML = 'Add';
 button.addEventListener("click");
+
 
 
 
